4b725f0f7aa52ff0314d1ed0e84388e86703e5d0
Sean Rooney <seandanielrooney@gmail.com>
2022-12-06 19:49:09 -0800

    adding more animation

--- randomGarbageTerminal.c
+++ randomGarbageTerminal.c
@@ -2,44 +2,91 @@
 #include <stdlib.h>
 #include <windows.h>
 
+// PRINTS THE INIT COUNTDOWN
+
+void initPrint()
+{
+    int countdownNumber = 3;
+
+    while (countdownNumber > 0)
+    {
+        /* code */
+        printf(" %d ", countdownNumber);
+
+        Sleep(100);
+
+        int initDotsNumber = 9;
+
+        while (initDotsNumber > 0)
+        {
+            printf(". ");
+            Sleep(100);
+            initDotsNumber--;
+        }
+
+        printf("\n");
+
+        countdownNumber--;
+    }
+}
+
+// PRINTS ELLIPSIS ANIMATION
+
+void printEllipsisAnimation()
+{
+    int printEllipsisAnimationDotsCount = 2;
+
+    for (size_t i = 0; i < printEllipsisAnimationDotsCount; i++)
+    {
+        printf(".");
+        Sleep(1000);
+    }
+    printf(".\n");
+    Sleep(400);
+}
+
+// QUICKLY PRINTS A LONG LINE OF DOTS
+
 void printFastLongLineOfDots()
 {
-    int count = 24;
+    int FastLongLineOfDotsCount = 24;
 
-    for (size_t i = 0; i < count; i++)
+    for (size_t i = 0; i < FastLongLineOfDotsCount; i++)
     {
         printf(".");
         Sleep(100);
     }
     printf(".\n");
-    Sleep(200);
 }
 
 void main()
 {
 
-#define NUMBER_OF_STRING 14
-#define MAX_STRING_SIZE 40
+#define NUMBER_OF_INIT_STATEMENTS 15
+#define INIT_STATEMENT_MAX_STRING_SIZE 40
 
-    char initStatement[NUMBER_OF_STRING][MAX_STRING_SIZE] =
+    char initStatement[NUMBER_OF_INIT_STATEMENTS][INIT_STATEMENT_MAX_STRING_SIZE] =
         {
-            "loading...",
-            "initializing...",
-            "connecting...",
-            "downloading...",
-            "uploading...",
-            "optimizing...",
-            "compressing...",
-            "logging in...",
-            "connecting file manager...",
-            "FTP connecting...",
-            "routing...",
-            "sending report...",
-            "refactoring...",
-            "installing...",
-        };
+            "loading",
+            "initializing",
+            "connecting",
+            "downloading",
+            "uploading",
+            "optimizing",
+            "compressing",
+            "logging in",
+            "connecting file manager",
+            "FTP connecting",
+            "routing",
+            "sending report",
+            "refactoring",
+            "installing",
+            "booting to drive"};
 
-    char contentStatement[NUMBER_OF_STRING][MAX_STRING_SIZE] =
+#define NUMBER_OF_CONTENT_STATEMENTS 14
+#define CONTENT_STATEMENT_MAX_STRING_SIZE 40
+
+    char contentStatement[NUMBER_OF_CONTENT_STATEMENTS][CONTENT_STATEMENT_MAX_STRING_SIZE] =
         {
             "adding necessary drivers",
             "recognizing device",
@@ -61,9 +108,11 @@ void main()
 
     while (i = 1)
     {
-        for (int i = 0; i < NUMBER_OF_STRING; i++)
+        initPrint();
+        for (int i = 0; i < i + 1; i++)
         {
-            printf("%s\n", initStatement[rand() % 7]);
+            printf("%s", initStatement[rand() % 7]);
+            printEllipsisAnimation();
             Sleep(1000);
             printf("%s\n", contentStatement[rand() % 7]);
             Sleep(200);
